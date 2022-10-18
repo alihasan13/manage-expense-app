4963af245cf7943a3bb0ab1f4c408af7a9be2c0e
Ali Hasan <alihasan13@users.noreply.github.com>
2022-10-18 23:06:17 +0600

    new features added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
- 
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const DUMMY_EXPENSES = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -26,11 +25,26 @@ function App() {
   return (
     <div className="App">
       <h1>code herer</h1>
-      <ExpenseItem title={DUMMY_EXPENSES[0].title} amount={DUMMY_EXPENSES[0].amount} date={DUMMY_EXPENSES[0].date}></ExpenseItem>
-      <ExpenseItem title={DUMMY_EXPENSES[1].title} amount={DUMMY_EXPENSES[1].amount} date={DUMMY_EXPENSES[1].date}></ExpenseItem>
-      <ExpenseItem title={DUMMY_EXPENSES[2].title} amount={DUMMY_EXPENSES[2].amount} date={DUMMY_EXPENSES[2].date}></ExpenseItem>
-      <ExpenseItem title={DUMMY_EXPENSES[3].title} amount={DUMMY_EXPENSES[3].amount} date={DUMMY_EXPENSES[3].date}></ExpenseItem>
-      
+      <ExpenseItem
+        title={DUMMY_EXPENSES[0].title}
+        amount={DUMMY_EXPENSES[0].amount}
+        date={DUMMY_EXPENSES[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={DUMMY_EXPENSES[1].title}
+        amount={DUMMY_EXPENSES[1].amount}
+        date={DUMMY_EXPENSES[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={DUMMY_EXPENSES[2].title}
+        amount={DUMMY_EXPENSES[2].amount}
+        date={DUMMY_EXPENSES[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={DUMMY_EXPENSES[3].title}
+        amount={DUMMY_EXPENSES[3].amount}
+        date={DUMMY_EXPENSES[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
