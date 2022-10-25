cdb8039f9e3a497db7233d3d1f765338e0cda476
Ali Hasan <alihasan13@users.noreply.github.com>
2022-10-25 23:52:25 +0600

    learn statefull or stateless components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log("Clicked!");
-  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>click here </button>
       </div>
     </Card>
   );
