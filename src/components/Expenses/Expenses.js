bb4cb6ff938c62aa41f440802ceb9675d8504b44
Ali Hasan <alihasan13@users.noreply.github.com>
2022-10-27 23:59:53 +0600

    dd

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2019");
   const yearChnageHandler = (selectedYear) => {
     setSelectedYear(selectedYear);
     console.log(selectedYear);
-    
   };
-  const filteredExpense = props.items.filter(expense=>{return expense.date.getFullYear().toString() === selectedYear })
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+ 
+
 
   return (
-    <div>
+    <li>
       <Card className="expenses">
         <ExpenseFilter
           yearSelected={selectedYear}
           onYearChange={yearChnageHandler}
         />
-        {filteredExpense.map((expense) => (
+          <ExpenseList items={filteredExpense}/>
 
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* {filteredExpense.length === 0 && <p>No Expenses Found</p>}
+        {filteredExpense.length > 0 &&  filteredExpense.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+        {/* {filteredExpense.length === 0 ? (
+          <p>No Expense Found </p>
+        ) : (
+          filteredExpense.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
       </Card>
-    </div>
+    </li>
   );
 };
 
